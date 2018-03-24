b1f2f8c9df52f169480bec5d70f0a3668ef8db27
Saurabh Singh <saurabh1065@gmail.com>
2018-03-24 18:41:28 +0530

    neg_pos.c

---
+++ pos_neg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+   scanf("%d",&num);
+   if(num>=0)
+   {
+       printf("POSITIVE");
+     }
+       else
+       {
+           printf("NEGATIVE");
+       }
+}
